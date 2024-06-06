258ec235e3f5ecc185282d5b6d198f2eb791dff5
Gyanendra <gyanen56@gmail.com>
2024-06-06 12:20:35 +1000

    adding user using controlled input

--- src/Hooks/8.ControlledInput.js
+++ src/Hooks/8.ControlledInput.js
@@ -1,16 +1,27 @@
 import { useState } from "react"
 
 const ControlledInput = () => {
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState(null)
+  const [users, setUsers] = useState([])
   const handleChange=(e)=>{
     const {name,  value} = e.target
     setUser({...user, [name]:value})
     console.log(user)
+    console.log(users)
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    if(user){
+      setUsers([...users, user])
+    }
+     
+    console.log(users.length, users)
   }
   return (
-    <div className=" card p-3">
-      <form action="">
-      
+    <>
+      <div className=" card p-3">
+        <h4>Add User</h4>
+        <form action="" onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="email" className="form-label">
               Email address
@@ -19,7 +30,7 @@ const ControlledInput = () => {
               type="email"
               className="form-control"
               aria-describedby="emailHelp"
-              name='email'
+              name="email"
               onChange={handleChange}
             />
           </div>
@@ -31,7 +42,7 @@ const ControlledInput = () => {
               type="password"
               className="form-control"
               id="password"
-              name='password'
+              name="password"
               onChange={handleChange}
             />
           </div>
@@ -39,9 +50,25 @@ const ControlledInput = () => {
           <button type="submit" className="btn btn-primary">
             Submit
           </button>
-      
-      </form>
-    </div>
+        </form>
+      </div>
+      {users.length>0?<User users={users} />:''}
+    </>
   )
 }
+
+
 export default ControlledInput
+
+  const User = ({users})=>{
+  return (<div className="p-5">
+   <h4>User Lists</h4>
+   <ul>
+    <li>Email Password</li>
+    {users.map((user, index)=>{
+      return <li key={index}>User Name: {user.email} Password : {user.password}</li>
+    })}
+   </ul>
+  </div>)
+}
+
