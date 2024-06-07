6426a201598a6f65f62eacd02915d851224b5bc2
Gyanendra <gyanen56@gmail.com>
2024-06-07 13:24:57 +1000

    select input in react

--- src/Hooks/9.CheckboxInput.js
+++ src/Hooks/9.CheckboxInput.js
@@ -1,21 +1,32 @@
 import { useState } from "react"
 
 const CheckboxInput = () => {
+    const frames = ['react', 'angular','vue', 'svelte']
     const [hello, setHello] = useState(false)
-
+    const [framework, setFramework] = useState('react')
     const handleCheck=(e)=>{
     
          setHello(e.target.checked)
          console.log(hello)
          console.log(e.target.checked)
     }
+    const handleFramework=(e)=>{
+        console.log(e.target.value)
+        setFramework(e.target.value)
+    }
   return (
     <div className="m-5">
-        <label htmlFor="test">Check</label>
-        <input type="checkbox" id="test" onChange={handleCheck} checked={hello} />
-        <p>hello</p>
-        {hello?"Hello":"Hi"}
-        
+      <label htmlFor="test">Check</label>
+      <input type="checkbox" id="test" onChange={handleCheck} checked={hello} />
+      <p>hello</p>
+      {hello ? 'Hello' : 'Hi'}
+      <div>
+        <select name="framework" id="framework" value={framework} onChange={handleFramework}>
+            {frames.map((frame,index)=>{
+                return <option  key={index} >{frame}</option>
+            })}
+        </select>
+      </div>
     </div>
   )
 }
