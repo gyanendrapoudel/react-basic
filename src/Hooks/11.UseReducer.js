508329f7fa9ec5c9f65944e03e977600884e65a6
Gyanendra <gyanen56@gmail.com>
2024-06-14 23:37:37 +1000

    after using useREdcuer HOOKs

--- src/Hooks/11.UseReducer.js
+++ src/Hooks/11.UseReducer.js
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { type } from "@testing-library/user-event/dist/type"
+import { useReducer, useState } from "react"
 
 const data = [
   { name: 'Bob', id: 1 },
@@ -6,25 +7,43 @@ const data = [
   { name: 'Peter', id: 3 },
   { name: 'Mark', id: 4 },
 ]
+const defaultState = {
+    users:data,
+}
+const reducer =(state, action)=>{
+ if (action.type === 'CLEAR_ALL') {
+   return { ...state, users: [] }
+ }
+ if(action.type ==="RESET"){
+   
+    return {...state, users:data}
+ }
+ if(action.type==="REMOVE_ITEM"){
+    const {id} = action.payload
+    const newUsers = state.users.filter((user) => user.id !== id)
+    return {...state, users:newUsers}
+ }
+}
 
 const UseReducer = () => {
- const [users, setUsers] = useState(data)
+ const [state, dispatch ] = useReducer(reducer, defaultState)
 
  const handleRemoveItem =(id)=>{
-   const newUsers = users.filter((user)=>user.id!==id)
-   setUsers(newUsers) 
+    dispatch({ type: 'REMOVE_ITEM' , payload:{id}})
+   
  }
 
  const handleClearAll = ()=>{
-    setUsers([])
+    dispatch({type:"CLEAR_ALL"})
  }
  const handleReset = ()=>{
-    setUsers(data)
+    dispatch({type:"RESET"})
  }
 
   return (
     <div className="mt-3 text-center">
-      {users.map((user, index) => {
+        {console.log(state)}
+      {state.users.map((user, index) => {
         const {id, name} = user
         return (
           <div key={index} className="fw-bolder  my-3 ">
@@ -33,7 +52,7 @@ const UseReducer = () => {
           </div>
         )
       })}
-        {users.length>0?
+        {state.users.length>0?
       <button className="btn btn-danger mt-3 w-25" onClick={handleClearAll}>Clear All</button>:
       <button className="btn btn-success mt-3 w-25" onClick={handleReset}>Reset</button>
 
